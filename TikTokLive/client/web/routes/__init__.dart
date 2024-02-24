b341cc75ff9f3c83a77f2144a0b2eabd0645efeb
Mohamed Shaamil <145243+theseaeagle@users.noreply.github.com>
2024-02-24 12:01:57 +0500

    Update and rename __init__.py to __init__.dart

---
+++ TikTokLive/client/web/routes/__init__.dart
@@ -0,0 +1,7 @@
+// Importing routes from different files
+import 'fetch_gift_list.dart';
+import 'fetch_image.dart';
+import 'fetch_room_id.dart';
+import 'fetch_room_info.dart';
+import 'fetch_sign.dart';
+import 'fetch_video.dart';
